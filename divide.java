a8ee04ca7ee11a5e556c7f78b404a76fc5220d6f
saumya <verma.saumya.703@gmail.com>
2023-05-12 17:13:26 +0530

    Day 2 Commit

---
+++ divide.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class divide {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int d=b/a;
+        System.out.println(d);
+    }
+}
